2bf95cd2d6980fe8fa33715a13712f2a7e371ab3
Fredrik Larsson <fredrik@wizardry.se>
2023-01-18 22:00:00 +0100

    Added log and env_logger crates, and started logging subscription requests.

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,19 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach it to the application state
